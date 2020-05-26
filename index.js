c0adc7b27f0eba6b2fbfb4f442c067687b95bb46
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-05-26 10:09:54 -0400

    Salvando alterações no projeto

--- index.js
+++ index.js
@@ -7,40 +7,7 @@ const mysql = require('mysql');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//definindo as rotas
-const router = express.Router();
-router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
-app.use('/', router);
-//pesquisa 
-router.get('/clientes/:id?', (req, res) =>{
-    let filter = '';
-    if(req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
-    execSQLQuery('SELECT * FROM Clientes' + filter, res);
-})
-//delete
-router.delete('/clientes/:id', (req, res) =>{
-    execSQLQuery('DELETE FROM Clientes WHERE ID=' + parseInt(req.params.id), res);
-})
-
-//inserir
-router.post('/clientes', (req, res) =>{
-    const nome = req.body.nome.substring(0,150);
-    const cpf = req.body.cpf.substring(0,11);
-    execSQLQuery(`INSERT INTO Clientes(Nome, CPF) VALUES('${nome}','${cpf}')`, res);
-});
-
-//alterar
-router.patch('/clientes/:id', (req, res) =>{
-    const id = parseInt(req.params.id);
-    const nome = req.body.nome.substring(0,150);
-    const cpf = req.body.cpf.substring(0,11);
-    execSQLQuery(`UPDATE Clientes SET Nome='${nome}', CPF='${cpf}' WHERE ID=${id}`, res);
-})
-
-//inicia o servidor
-app.listen(port);
-console.log('API funcionando!');
-
+//Configurando connexão com o banco de dados
 function execSQLQuery(sqlQry, res){
     const connection = mysql.createConnection({
         host     : 'localhost',
@@ -59,3 +26,40 @@ function execSQLQuery(sqlQry, res){
         console.log('executou!');
     });
   }
+
+//definindo as rotas
+const router = express.Router();
+
+/* //inserir
+router.post('/clientes', (req, res) =>{
+    const nome = req.body.nome.substring(0,150);
+    const cpf = req.body.cpf.substring(0,11);
+    execSQLQuery(`INSERT INTO Clientes(Nome, CPF) VALUES('${nome}','${cpf}')`, res);
+}); */
+
+router.get('/', (req, res) => res.json({ message: 'Tela inicial sisficha!' }));
+app.use('/', router);
+
+//delete
+router.delete('/clientes/:id', (req, res) =>{
+    execSQLQuery('DELETE FROM Clientes WHERE ID=' + parseInt(req.params.id), res);
+})
+
+//pesquisa 
+router.get('/clientes/:id?', (req, res) =>{
+    let filter = '';
+    if(req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM Clientes' + filter, res);
+})
+//alterar
+router.patch('/clientes/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    const nome = req.body.nome.substring(0,150);
+    const cpf = req.body.cpf.substring(0,11);
+    execSQLQuery(`UPDATE Clientes SET Nome='${nome}', CPF='${cpf}' WHERE ID=${id}`, res);
+})
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
+
